75cc6b2f641451d414b753570c5d56eef416bf50
Santiago Sánchez Taborda <santiago.sanchez.t@gmail.com>
2022-03-16 22:24:40 -0500

    Chore/monorepo config (#1)
    * chore(lerna): Add monorepo base configurations
    
    * chore(counter1): Add webpack and typescrip configurations
    
    * fix(webpack): Modify exposes property in webpack

---
+++ apps/counter1/webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const dependencies = require("./package.json").dependencies;
+
+module.exports = {
+	entry: "./src/index.ts",
+	mode: "development",
+	devServer: {
+		port: 3001,
+		open: true,
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx|tsx|ts)$/,
+				loader: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		}),
+		new ModuleFederationPlugin({
+			name: "counterOne",
+			filename: "remoteEntry.js",
+			exposes: {
+				"./CounterOne": "./src/components/CounterOne",
+			},
+			shared: {
+				...dependencies,
+				react: { singleton: true, eager: true, requiredVersion: dependencies.react },
+				"react-dom": {
+					singleton: true,
+					eager: true,
+					requiredVersion: dependencies["react-dom"],
+				},
+			},
+		}),
+	],
+};
